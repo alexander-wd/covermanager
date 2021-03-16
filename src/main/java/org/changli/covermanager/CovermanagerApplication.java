6a6ca881d98f75f16272fcb8ab9c265333d2c02f
alex <1375252471@qq.com>
2021-03-16 20:41:41 +0800

    thymeleaf

--- src/main/java/org/changli/covermanager/CovermanagerApplication.java
+++ src/main/java/org/changli/covermanager/CovermanagerApplication.java
@@ -10,6 +10,7 @@ public class CovermanagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CovermanagerApplication.class, args);
+		System.out.println("******************************启动成功********************************");
 	}
 
 }
